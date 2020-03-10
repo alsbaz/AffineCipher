7f291e98c63ed422ad992c5d57e5eb61a4cc7996
alsbaz <balazs.toth97@gmail.com>
2020-03-10 15:20:31 +0000

    Added notes, and option to see all options

--- BreakAffineCipher.py
+++ BreakAffineCipher.py
@@ -1,9 +1,12 @@
 import math;
-import time;
-import msvcrt;
 #Using math module again for greatest common divider.
+import time;
+#For testing purposes, i was monitoring efficient ways of running it.
 
 array = (open("dictionary.txt").read()).split("\n")
+#This line imports the dictionary file and reads it, splits each item by the new line
+#And assigns each new line to the array as a seperate element, creating a big array with words to be used later.
+
 
 def crackAffine(m):
     plainText = '';
@@ -14,7 +17,8 @@ def crackAffine(m):
     encryptText = encryptText.upper();
 
     start_time = time.time();
-
+    #Sets start time here (time.time() is the current time).
+    
     length = len(encryptText);
     #Simular to decryption however only asks for the encrypted text, not the keys.
     
@@ -51,25 +55,34 @@ def crackAffine(m):
                     plainText += chr(encryptNum);
                     counter += 1;
                 #Every other character gets discarded.
-
-            ##dictionaryCheck(plainText);
             
             match = dictionaryCheck(plainText);
+            #Assign the returned class object 
             if match.weight != 0:
+                #Check the weight value of class and goes into if statement if not 0.
                 matchList.append(match);
-                
+                #Appends the instance of the class to matchList, which keeps all the possible ones which triggered
                 
             
     sortedList = sorted(matchList, key=lambda i: i.weight, reverse=True);
+    #Make a new list from matchList, which gets organised in descending order
+    #Lambda is a anonymous function, it takes the weight value and organises by them.
 
     print("My program took", time.time() - start_time, "to run")
+    #This is the end of timeing. If we go on for longer, the inputs make the time very extended
+    #And not even neceserry after, as not many calculations, functions happening after this.
 
     for z in sortedList:
         print(z.text, "with a weight of", z.weight);
-        trigger = input("Enter \"Exit\" if you found it, or press enter to continue:\n")
+        #For loop goes through the sorted list from the one with highest weight, and prints them one by one.
+        trigger = input("Enter \"Exit\" if you found it, or press enter to continue or enter \"All\" for all results:\n")
         if trigger == "Exit":
             break;
-
+        elif trigger == "All":
+            for x in sortedList:
+                print(x.text, "with a weight of", x.weight);
+                #Gives the option to see all the words which matched instead of going through them one by one.
+            break
 
     
 def inverse(a, m):
@@ -88,17 +101,24 @@ def inverse(a, m):
     #Returns the remainder. This is to make sure it is not a negative number or a large number. 
 
 def dictionaryCheck(plain):
+    #This function is used to compare the strings which go through brute force to the dictionary array.
     lenCounter = 0;
     for i in array:
+        #Takes each word from the array one by one
         if i in plain:
+            #If the word is in the string
             lenCounter += len(i)*len(i);
+            #Adds the number of letters squared to the counter
     
     class textCounter:
+        #Made a class to contain the counter and text to be able to return to other function
         def __init__(self):
+            #Init is a reserved method in classes, a constructor. Specify all the values in it by self.value 
             self.text = plain
             self.weight = lenCounter
 
-    return textCounter();    
+    return textCounter();
+    #Return it to function.
 
 def main():
     m = 26;
